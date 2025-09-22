00c0cf7afdfe4e488d5528321b9cb87f14a2e8d3
drkbluescience <zngnenise@gmail.com>
2025-09-22 13:54:42 +0300

    Add Capacitor Android support for mobile app deployment

---
+++ android/app/src/main/java/com/zsu/dernek/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zsu.dernek;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zsu.dernek',
+  appName: 'ZSU Dernek',
+  webDir: 'dist'
+};
+
+export default config;
